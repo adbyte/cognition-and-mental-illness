7353009ecedee96e1c65e558760cbb44d7022df2
adbyte <35404279+adbyte@users.noreply.github.com>
2024-07-18 17:09:05 +0530

    Update User.js

--- User.js
+++ User.js
@@ -1,3 +1,4 @@
+/*
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
@@ -10,3 +11,4 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
+*/
