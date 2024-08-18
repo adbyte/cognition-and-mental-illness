c7e88cb58f34fb3644f7f3b5a592eb3d7da2df45
MAYER GOYAL <goyalmayer2@gmail.com>
2024-08-19 03:42:23 +0530

    complete sql fetching and retrival of user data

--- react-comp/src/AdminDashBoard/AdminDashboard.jsx
+++ react-comp/src/AdminDashBoard/AdminDashboard.jsx
@@ -1,22 +1,50 @@
 import React, { useEffect, useState } from "react";
+import "./AdminDashboard.css";
 const AdminDashboard = () => {
   const [users, setusers] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:3001/users")
       .then((response) => response.json())
-      .then((data) => setusers(data))
+      .then((data) => {
+        console.log("Data received:", data);
+        setusers(data);
+      })
       .catch((error) => console.error("Error:", error));
   }, []);
+
+  useEffect(() => {
+    console.log("Users state updated:", users); // Log users state when it's updated
+  }, [users]); // Watch for changes to the users state
+  const link = "(link unavailable)";
   return (
     <div>
       <h1>Admin Dashboard</h1>
       <h2>Users</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.username}</li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>name</th>
+            <th>age</th>
+            <th>gender</th>
+            <th>education</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.username}</td>
+              <td>{user.age}</td>
+              <td>{user.gender}</td>
+              <td>{user.education}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
+
 export default AdminDashboard;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -18,26 +19,41 @@ db.connect((err) => {
 });
 
 app.post("/store-data", (req, res) => {
-  const { username, password, email, age, gender, education, job_industry } =
+  const { username, password, emailid, age, gender, education, job_industry } =
     req.body;
   const useQuery =
-    "INSERT INTO users (username , password , email ,age , gender , education , job_industry ) VALUES (? , ? ,? ,? ,?,?,?)";
+    "INSERT INTO users (username , password , emailid ,age , gender , education , job_industry ) VALUES (? , ? ,? ,? ,?,?,?)";
   db.query(
     useQuery,
-    [username, password, email, age, gender, education, job_industry],
+    [username, password, emailid, age, gender, education, job_industry],
     (err, result) => {
-      if (err) throw err;
-      const userId = result.insertId;
-      res.send("data stored in sql");
+      if (err) {
+        console.error(err);
+        res.status(500).send({ message: "Error storing data" });
+      } else {
+        const userId = result.insertId;
+        res.send("Data stored in SQL");
+      }
     }
   );
 });
+
 app.get("/users", (req, res) => {
+  console.log("Executing /users endpoint");
   const query = "SELECT * FROM users";
   db.query(query, (err, result) => {
-    if (err) throw err;
-    res.json(result);
+    if (err) {
+      console.error("Error retrieving data:", err);
+      res.status(500).send({ message: "Error retrieving data" });
+    } else {
+      console.log("Data retrieved:", result); // Add this log
+      res.json(result);
+    }
   });
+});
+app.get("*", (req, res) => {
+  console.log("Received request:", req.url);
+  res.send("Request received");
 });
 
 app.listen(3001, () => {
